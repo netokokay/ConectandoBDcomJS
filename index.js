c87b81710a792db56ae830c7ec66bcb0b02915b8
Neto Kokay <netokokay@gmail.com>
2022-09-07 22:53:18 -0300

    Conexão do JS ao PostgreSQL

---
+++ index.js
@@ -0,0 +1,37 @@
+const Client = require('pg').Client
+const cliente = new Client({
+    user: "postgres",
+    password: "adminadmin",
+    host: "127.0.0.1",
+    port: 5432,
+    database: "teste"
+})
+
+/*
+cliente.connect()
+cliente.query("select * from clientes")
+.then(results => {
+    const resultado = results.rows
+    console.table(resultado)
+    console.log("Conexão efetuada com sucesso")
+})
+.finally(() => cliente.end())
+*/
+
+getClientes()
+
+async function getClientes(){
+    try{
+    console.log("Iniciando a conexão com o Banco de Dados.")
+    await cliente.connect()
+    console.log("Conexão efetuada com sucesso!")
+    const resultado = await cliente.query("select * from clientes")
+    console.table(resultado.rows)    
+}
+catch(e){
+console.log("Ocorreu um erro na conexão com o Banco de Dados, erro: "+ e)
+}
+finally{
+    await cliente.end()
+    console.log("Desconectado do Banco de Dados")
+}}
